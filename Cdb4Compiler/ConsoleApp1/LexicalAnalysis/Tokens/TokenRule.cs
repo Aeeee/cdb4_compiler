e38e70b8603aa7013505dd83eb48d759bb36c7a1
Andrej Deriabin <aderiabin.98@gmail.com>
2020-03-28 16:38:42 +0300

    Implemented token pattern definition.

---
+++ Cdb4Compiler/ConsoleApp1/LexicalAnalysis/Tokens/TokenRule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cdb4Compiler.LexicalAnalysis.Tokens
+{
+    class TokenRule
+    {
+        public TokenType Type { get; private set; }
+        public string Pattern { get; private set; }
+
+        public TokenRule(TokenType type, string pattern)
+        {
+            Type = type;
+            Pattern = pattern;
+        }
+    }
+}
+
---
+++ Cdb4Compiler/ConsoleApp1/LexicalAnalysis/Tokens/TokenRules.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Cdb4Compiler.LexicalAnalysis.Tokens
+{
+    class TokenRules
+    {
+        private static TokenRule[] rules = new TokenRule[]
+        {
+            new TokenRule(TokenType.SPECIAL_SYMBOL, ";|,"),
+            new TokenRule(TokenType.KEYWORD, "VAR|IF|THEN|ELSE"),
+            new TokenRule(TokenType.PARENTHESES, "\\(|\\)"),
+            new TokenRule(TokenType.COMPARSION_OP, ">|<|=="),
+            new TokenRule(TokenType.SUMM_DIFF_OP, "+|-"),
+            new TokenRule(TokenType.MUL_DIV_OP, "*|/"),
+            new TokenRule(TokenType.ASSIGNMENT_OP, ":="),
+            new TokenRule(TokenType.CONSTANT, "[0-9]+"),
+            new TokenRule(TokenType.IDENTIFIER, "[a-z]+")
+        };
+
+        public int GetMatchCount(string input)
+        {
+            int matches = 0;
+            foreach (var rule in rules)
+                if (Regex.IsMatch(input, rule.Pattern))
+                    matches += 1;
+            return matches;
+        }
+
+        public Token CreateToken(string input, int line, int column)
+        {
+            var rule = GetFirstMatch(input);
+            if (rule == null)
+                throw new ArgumentException("Input does not match any token rules.");
+            return new Token(rule.Type, input, line, column);
+        }
+
+        private TokenRule GetFirstMatch(string input)
+        {
+            foreach (var rule in rules)
+                if (Regex.IsMatch(input, rule.Pattern))
+                    return rule;
+            return null;
+        }
+
+    }
+}
